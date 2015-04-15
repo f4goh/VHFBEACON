5adf1a9eea1e3d3126ed3ea80c77453a065b5ccc
f4goh <f4goh@orange.fr>
2015-04-15 21:47:55 +0200

    Create fox.ino

---
+++ examples/fox/fox.ino
@@ -0,0 +1,69 @@
+/********************************************************************************************
+ * VHF multimodes beacon Arduino exemple
+ * Created 28/02/2015
+ * Anthony LE CREN f4goh@orange.fr 
+ * Use this code freely
+ * Send modulate carrier fréquency every seconds
+ *******************************************************************************************/
+
+#include <AD9850SPI.h>
+#include <VHFBEACON.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <avr/pgmspace.h>
+
+#define QRG 144800000/8
+
+
+int freeRam () 
+{
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+
+
+
+void setup() {
+   Serial.begin(57600);
+   Serial.print(F("Free RAM : "));
+   Serial.println(freeRam());
+   DDS.begin(13, 9, 8);   // in order pin W_CLK, FQ_UD et RESET
+    DDS.calibrate(125001100);
+   Beacon.begin(4, 5, 6, 7);   //in order pin sync, led (fix gps), mod et cde
+    Beacon.modulation(1);   //level 1 to enable ics511
+    Beacon.ddsPower(0);   // power down
+ }
+
+void loop() {
+   Serial.flush();   //purge Serialout before txing
+    cli();   //disable interrupt before txing
+    Beacon.ddsPower(1);   //dds Power on
+    DDS.setfreq(QRG, 0);   // QRG fréquency
+    carrier();   //modulate carrier
+    Beacon.ddsPower(0);   //dds power off
+    sei();   //enable interrupt for  serials
+    Serial.println("TX Done");   //Roger it's done
+    delay(1000L);
+}
+
+void carrier() {
+   for (int n =0; n <500; n ++) {
+      Beacon.modulation(1);      //level 1 to enable ics511
+       delay(2);
+      Beacon.modulation(0);      //level 1 to disable ics511
+       delay(2);
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
