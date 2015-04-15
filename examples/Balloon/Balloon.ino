d1a628068621804267b76a875f551c27ad808910
f4goh <f4goh@orange.fr>
2015-04-15 21:43:33 +0200

    Update Balloon.ino

--- examples/Balloon/Balloon.ino
+++ examples/Balloon/Balloon.ino
@@ -1,4 +1,10 @@
-// Version BTS touchard sans gps avec acquisition de chaine serie en provenance de la carte ST
+/********************************************************************************************
+ * VHF multimodes beacon Arduino exemple
+ * Created 28/02/2015
+ * Anthony LE CREN f4goh@orange.fr 
+ * Use this code freely
+ * Send APRS Sentence every 10 seconds with télémetry
+ *******************************************************************************************/
 
 #include <AD9850SPI.h>
 #include <VHFBEACON.h>
@@ -7,40 +13,29 @@
 #include <SoftwareSerial.h>
 #include <avr/pgmspace.h>
 
+#define txPin 3      //tx pin in GPS connection
+#define rxPin 2      //rx pin in GPS connection
+
+SoftwareSerial gps = SoftwareSerial(rxPin, txPin);
 
 
 // sounding ballon char array 
-unsigned char   balloon[116]={'T'<<1,'M'<<1,'B'<<1,'S'<<1,'T'<<1,' '<<1,0x60,             //avant APTT4 7 octets (0-6) call spécial TM72BST ou TMBST
-                           'F'<<1,'6'<<1,'K'<<1,'F'<<1,'I'<<1,' '<<1,('0' + 12) << 1,     //F6KFI-12 7 octets (7-13)
+unsigned char   balloon[116]={'F'<<1,'4'<<1,'G'<<1,'O'<<1,'H'<<1,' '<<1,0x60,              //avant APTT4 7 octets (0-6)
+                           'F'<<1,'6'<<1,'K'<<1,'F'<<1,'I'<<1,' '<<1,('0' + 11) << 1,     //F4GOH-11 7 octets (7-13)
                            'W'<<1,'I'<<1,'D'<<1,'E'<<1,'1'<<1,' '<<1,('0' + 1) << 1,      //WIDE1-1 7 octets (14-20)
                            'W'<<1,'I'<<1,'D'<<1,'E'<<1,'2'<<1,' '<<1,('0' + 1) << 1 | 1 , //WIDE2-1   fin ssid lsb =1 7 octets (21-27)
                            0x03,0xf0,                                                     //ctrl, pid 2 octets (28-29)
-                           // A partir d'ici le champ de données, construite et récupérée à partir de la carte ST
                            '/','1','5','0','4','5','2','h',      //heure 8 (30-37)
-                           '4','7','5','3','.','4','1','N','/','0','0','0','1','6','.','6','0','E',      //lat, long 18 octets (38-55)
-                           '>','1','8','0',    //Course degrees (56-59)
-                           '/','0','0','5',    //Gps speed knots (60-63)
+                           '4','8','5','1','.','2','0','N','/','0','0','2','2','0','.','9','2','E',      //lat, long 18 octets (38-55)
+                           '>','1','8','0',    //Course degrees (56-59)    not implemented in this exemple
+                           '/','0','0','5',    //Gps speed knots (60-63)   not implemented in this exemple
                            '/','A','=','0','7','2','9','6','6',  // Altitude (feet) (64-72)
-                           '/','A','1','=','0','0','0',  // Télémetry 0 (73-79)
-                           '/','A','2','=','0','0','0',  // Télémetry 1 (80-86)
-                           '/','A','3','=','0','0','0',  // Télémetry 2 (87-93)
-                           '/','A','4','=','0','0','0',  // Télémetry 3 (94-100)
-                           '/','A','5','=','0','0','0',  // Télémetry 4 (101-107)
+                           '/','A','0','=','0','0','0',  // Télémetry 0 (73-79)
+                           '/','A','1','=','0','0','0',  // Télémetry 1 (80-86)
+                           '/','A','2','=','0','0','0',  // Télémetry 2 (87-93)
+                           '/','A','3','=','0','0','0',  // Télémetry 3 (94-100)
+                           '/','A','4','=','0','0','0',  // Télémetry 4 (101-107)
                            '/',' ','B','a','l','l','o','n'};   //comment 15 car octets (108-115)
-
-//  tester la(les) chaine(s) suiavante(s) dans le moniteur série en modifiant les caracères :
-//  /150452h4753.41N/00016.60E>180/005/A=072966/A1=000/A2=000/A3=000/A4=000/A5=000/ Ballon
-//  /160020h4775.41N/00001.60W>190/010/A=010000/A1=000/A2=000/A3=000/A4=000/A5=000/ Ballon
-//  /172025h4801.41N/00028.60E>270/020/A=020540/A1=255/A2=000/A3=000/A4=000/A5=000/ Ballon
-//  /180452h5020.41N/00056.60W>090/030/A=030258/A1=000/A2=255/A3=000/A4=000/A5=000/ Ballon
-
-//adapter le nombre et la longueur des telemesures à votre besoin en respectant la syntaxe (entre /A1 et / Ballon)
-//attention plus la chaine est longue plus il risque d'y avoir des erreurs de transmission
-
-
-String DataFromST;
-
-int n;
 
 
 int freeRam () 
@@ -51,77 +46,93 @@ int freeRam ()
 }
 
 
-void setup(){
 
-Serial.begin(57600);
-
-//Serial.print(F("Free RAM : "));      //pour info 
-//Serial.println(freeRam());
- 
-DDS.begin(13,9,8);  // in order pin W_CLK, FQ_UD et RESET
-DDS.calibrate(125001100);
- 
- Beacon.begin(4,5,6,7); //in order pin sync, led (fix gps), mod et cde
- 
- Beacon.modulation(1);    //level 1 to enable ics511
- 
- Beacon.freq=(144650000)/8;    // Aprs fréquency for balloon
- Beacon.ddsPower(0);          // power down
-
-
-}
-
-
-/*boucle principale
-1 En attente de données SERIE en provenance de la carte ST
-attention données SERIES 0/5V compatible TTL
-2 Lecture d'un chaine jusqu'a nouvelle ligne \n
-3 copie dans le tableau d' unsigned char
-4 purge serial+ interruption HS
-5 Mise en route du dds
-6 envoi des donnes
-7 Dds off
-8 interruption en service
-9 info TXING 
-
-remaque : l'intervale de temps conseillé (de 20 secondes) entre 2 emissions se fait grace à la carte ST
-et la réception de données Séries
-
-*/
-
-void loop(){
-
- if (Serial.available()){
-  DataFromST = Serial.readStringUntil('\n');  
-  Serial.print(DataFromST.length());    //acq to ST data length
-  
-  for (n=0;n<DataFromST.length();n++) {
-                                          balloon[30+n]=DataFromST[n];
-                                          //Serial.print((char) balloon[30+n]);      //verify
-                                        }
- //verifications
- /*  
- Serial.println(DataFromST);
-
- for(n=0;n<sizeof(balloon);n++) {      //print char array track (for info)
-                                 Serial.print(balloon[n],HEX);
-                                 Serial.print(":");
-                                 Serial.print((char)balloon[n]);
-                                 Serial.print(",");
-                               }    
- Serial.println();
-  */
- 
-  Serial.flush();                          //purge Serialout before txing
-  cli();                                   //disable interrupt before txing
-  Beacon.ddsPower(1);                      //dds Power on
-  Beacon.sendpacket(balloon,sizeof(balloon));                                                 
-  Beacon.ddsPower(0);                      //dds power off
-  sei();                                   //enable interrupt for serials
-  Serial.println("TX Done");               //Roger it's done (optional)
-
+void setup() {
+   Serial.begin(57600);
+   pinMode(rxPin, INPUT);
+   pinMode(txPin, OUTPUT);
+   Serial.print(F("Free RAM : "));
+   Serial.println(freeRam());
+   DDS.begin(13, 9, 8);   // in order pin W_CLK, FQ_UD et RESET
+    DDS.calibrate(125001100);
+   Beacon.begin(4, 5, 6, 7);   //in order pin sync, led (fix gps), mod et cde
+    Beacon.GPGGA.pperiod =10;   //interval between two tx
+    Beacon.GPGGA.debug = true;   //allow debug print char
+    Beacon.GPGGA.dumpNmea = false;   //allow print nmea sentence
+    Beacon.modulation(1);   //level 1 to enable ics511
+    Beacon.mode =0;   //0: fm mode  1:ssb mode 
+    Beacon.freq = (144800000) /8;   // Aprs fréquency
+    Beacon.ddsPower(0);   // power down
+    gps.begin(4800);   //set up baud speed for GPS
+   //gps.begin(9600);
  }
- 
+
+void loop() {
+   if (gps.available()) Beacon.gpsnmea(gps.read());   //if  char into software serial decode nmea sentence
+    if (Beacon.GPGGA.sync ==1) // test if  is time to txing
+    {
+      Beacon.GPGGA.sync =0;      //txing sequence, so sync done
+       /*
+                   Serial.println( Beacon.GPGGA.hour);          //print some GPS info
+                   Serial.print( Beacon.GPGGA.Latitude);
+                   Serial.println( Beacon.GPGGA.NS);
+                   Serial.print( Beacon.GPGGA.Longitude);
+                   Serial.println( Beacon.GPGGA.EO);
+                   Serial.println( Beacon.GPGGA.fix);
+                   Serial.println( Beacon.GPGGA.altitude);        //altitude meter in char array
+                   Serial.println( Beacon.GPGGA.altidudeMeters);  //altitude meter in long
+                   Serial.println( Beacon.GPGGA.altidudeFeet);    //altitude feet in long
+                   Serial.println( Beacon.GPGGA.feet);            //altitude feet in char array
+                   */
+       memcpy(balloon +31, Beacon.GPGGA.hour, 6);      //prepare char array balloon to send
+       memcpy(balloon +38, Beacon.GPGGA.Latitude, 7);      //beware index number from char array
+       balloon[45] = Beacon.GPGGA.NS;
+      memcpy(balloon +47, Beacon.GPGGA.Longitude, 8);
+      balloon[55] = Beacon.GPGGA.EO;
+      memcpy(balloon +67, Beacon.GPGGA.feet, 6);
+      telemetry();
+      for (int n =0; n < sizeof(balloon); n ++) {        //print char array balloon (for  info)
+         Serial.print((char) balloon[n]);
+         Serial.print(",");
+      }
+       Serial.println();
+      Serial.flush();      //purge Serialout before txing
+       cli();      //disable interrupt before txing
+       Beacon.ddsPower(1);      //dds Power on
+       Beacon.sendpacket(balloon, sizeof(balloon));      //send packet
+       Beacon.ddsPower(0);      //dds power off
+       sei();      //enable interrupt for  serials
+       Serial.println("TX Done");      //Roger it's done
+    }
 }
 
-  
+void telemetry() {
+   int An0 = analogRead(A0) /4;   //convert to 8 bits 4 analog channels
+   int An1 = analogRead(A1) /4;
+   int An2 = analogRead(A2) /4;
+   int An3 = analogRead(A3) /4;
+   int2char(An0, 77);
+   int2char(An1, 84);
+   int2char(An2, 91);
+   int2char(An3, 98);
+}
+
+void int2char(int value, int offsetWx) {
+   char measures[4];
+   String str;   //declaring string
+   str = String(value);   //converting integer into a string
+   Serial.println(str);
+   str.toCharArray(measures, 4);   //passing the value of the string to the character array
+   memcpy(balloon + offsetWx, measures, 3);   //prepare char array balloon to send
+}
+
+
+
+
+
+
+
+
+
+
+
